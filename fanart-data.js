eb44c0ad4c0d5f0fe436fd021839eeca2841df7c
naninanijapon <Kokkishin@protonmail.com>
2025-05-27 22:27:27 +0900

    ファイル整理

--- fanart-data.js
+++ fanart-data.js
@@ -1,59 +1,59 @@
 /*
   【ファンアートの追加方法】
-  1. 新しい画像ファイルを `naniverso` フォルダに置きます。
+  1. 新しい画像ファイルを `naniverso/assets/fanart/` フォルダに置きます。 // ✨コメント修正✨
   2. 下のリストに、新しいブロック {...} をコピーして追加します。
-  3. `image:` の "..." に画像ファイル名を、
+  3. `image:` の "..." に "assets/fanart/画像ファイル名" を、 // ✨コメント修正✨
      `artist:` の "..." に作者名（XのIDなど）を記入します。
 */
 
 const fanArtData = [
   // 下のファイルから順番に記述
   {
-    image: "GrzjAEFWYAAW-c2.jpeg",
+    image: "assets/fanart/GrzjAEFWYAAW-c2.jpeg", // ✨パス修正✨
     artist: "@GrzjAEFWYAAW-c2"
   },
   {
-    image: "ivanvaldez1589MKL=00.png",
+    image: "assets/fanart/ivanvaldez1589MKL=00.png", // ✨パス修正✨
     artist: "@ivanvaldez1589MKL"
   },
   {
-    image: "MetalSulfurStudios.png",
+    image: "assets/fanart/MetalSulfurStudios.png", // ✨パス修正✨
     artist: "@MetalSulfurStudios"
   },
   {
-    image: "KATT_Anongaka.png",
+    image: "assets/fanart/KATT_Anongaka.png", // ✨パス修正✨
     artist: "@KATT_Anongaka"
   },
   {
-    image: "ivanvaldez1589=01.png",
+    image: "assets/fanart/ivanvaldez1589=01.png", // ✨パス修正✨
     artist: "@ivanvaldez1589"
   },
   {
-    image: "Daro_oficial.png",
+    image: "assets/fanart/Daro_oficial.png", // ✨パス修正✨
     artist: "@Daro_oficial"
   },
   {
-    image: "ajstudios6756.png",
+    image: "assets/fanart/ajstudios6756.png", // ✨パス修正✨
     artist: "@ajstudios6756"
   },
   {
-    image: "JoshuaGonzalez-Otaku18_587.png",
+    image: "assets/fanart/JoshuaGonzalez-Otaku18_587.png", // ✨パス修正✨
     artist: "@JoshuaGonzalez-Otaku18_587"
   },
   {
-    image: "ivanvaldez1589=03.png",
+    image: "assets/fanart/ivanvaldez1589=03.png", // ✨パス修正✨
     artist: "@ivanvaldez1589"
   },
   {
-    image: "@_N4hkz_.jpeg",
+    image: "assets/fanart/@_N4hkz_.jpeg", // ✨パス修正✨
     artist: "@_N4hkz_"
   },
   {
-    image: "ZarateLX.jpeg",
+    image: "assets/fanart/ZarateLX.jpeg", // ✨パス修正✨
     artist: "@ZarateLX"
   },
   {
-    image: "Hans_Eler.jpeg",
+    image: "assets/fanart/Hans_Eler.jpeg", // ✨パス修正✨
     artist: "@Hans_Eler"
   },
   // ここに新しいファンアートを追加
